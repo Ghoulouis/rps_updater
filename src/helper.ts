c2eeef2776eaf2fb049c0c80fbce0177076783a5
Ghoulouis <maikhanh3241@gmail.com>
2025-08-26 16:55:24 +0700

    update script for show apr

--- src/helper.ts
+++ src/helper.ts
@@ -1,9 +1,9 @@
 export async function getTotalTVL() {
-  return 18000;
+  return 30000;
 }
 
 export async function getPrice(symbol: string) {
-  if (symbol == "ETH") return 4374;
+  if (symbol == "ETH") return 4200;
   if (symbol == "USDC") return 1;
   if (symbol == "USDT") return 1;
   return 0;
--- src/main.ts
+++ src/main.ts
@@ -20,10 +20,10 @@ const tokens = [
     decimals: 6,
   },
 ];
-let precision = 10n ** 18n;
+let precision = 10n ** 24n;
+
 async function getRpsForToken(apy: number, symbol: string) {
   let token = tokens.find((t) => t.symbol === symbol);
-
   let price = await getPrice(symbol);
   let priceBigInt = parseUnits(price.toFixed(incentiveToken.decimals), incentiveToken.decimals);
 
@@ -35,24 +35,38 @@ async function getRpsForToken(apy: number, symbol: string) {
   return rps;
 }
 
+async function getAPRfromRps(rps: bigint, symbol: string) {
+  let token = tokens.find((t) => t.symbol === symbol);
+
+  let rewardUSDTATokenAYear = (rps * 365n * 24n * 3600n * parseUnits("1", token!.decimals)) / precision;
+
+  let priceToken = await getPrice(symbol);
+
+  let priceUSDTToken = parseUnits(priceToken.toFixed(incentiveToken.decimals), incentiveToken.decimals);
+  console.log("priceUSDTToken", priceUSDTToken);
+  console.log("reward usdt", rewardUSDTATokenAYear);
+  let apy = Number(rewardUSDTATokenAYear * 100n) / Number(priceUSDTToken);
+  console.log(" APY", apy.toFixed(2));
+}
+
 async function main() {
   let tvl = await getTotalTVL();
   let apy = 100;
   if (tvl > 20000) {
     apy = (20000 / tvl) * 100;
-
-    console.log("apy", apy);
   }
+  console.log("apy", apy);
   let rpsETH = await getRpsForToken(apy, "ETH");
   await getRpsForToken(apy, "USDT");
   let rpsUSDC = await getRpsForToken(apy, "USDC");
 
-  let precision = 10n ** 18n;
   let rewardUSDCInAYear = (rpsUSDC * 365n * 24n * 3600n * 1_000_000n) / precision;
   console.log("reward One USDC In A Year", formatUnits(rewardUSDCInAYear, incentiveToken.decimals));
 
   let rewardETHInAYear = (rpsETH * 365n * 24n * 3600n * parseEther("1")) / precision;
   console.log("reward One USDC In A Year", formatUnits(rewardETHInAYear, incentiveToken.decimals));
+
+  await getAPRfromRps(rpsETH, "ETH");
 }
 
 main();
